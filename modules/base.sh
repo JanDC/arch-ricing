fb6788fbc1feb0d93ac00d394f60e0c5cd8fe5d8
Jan Decavele <jandecavele@gmail.com>
2019-10-12 20:14:26 +0200

    vagrant stuff

--- modules/base.sh
+++ modules/base.sh
@@ -3,8 +3,8 @@ echo 'provisioning base system (could be optional)'
 
 #TODO: user creation, keyboard, pacman config, installation of git e.a.
 pacman-mirrors -g
-pacman -Syyu
-pacman -S --noconfirm git xorg-server base-devel xorg-xinit zsh alacritty
+pacman -Syyu --noconfirm
+pacman -S --noconfirm git xorg-server base-devel xorg-xinit zsh alacritty python xf86-video-vesa
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 su $username -c "mkdir -p /tmp/trizen"
 su $username -c "git clone https://aur.archlinux.org/trizen.git /tmp/trizen"
--- modules/zsh.sh
+++ modules/zsh.sh
@@ -3,9 +3,9 @@
 pacman -S --noconfirm zsh zsh-completions
 
 # Install zsh
-su $username -c "trizen -Sy --noconfirm antibody screenfetchi neofetch"
+su $username -c "trizen -Sy --noconfirm antibody screenfetch neofetch"
 
 
-#su $username -c 'sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+su $username -c 'sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
 su $username -c "cp ${scriptlocation}/config/zsh/.zshrc ${U_HOME}/.zshrc"
 su $username -c "cp ${scriptlocation}/config/zsh/.zprofile ${U_HOME}/.zprofile"
